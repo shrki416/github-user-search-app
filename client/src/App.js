7cb45a251d1e796380be18eb80f79ba8d04e20d8
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-06 18:57:31 -0400

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import moon from "./assets/icon-moon.svg";
+import search from "./assets/icon-search.svg";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Github-user-search-app</h1>
+    <div className="container">
+      <div>
+        <h1>devfinder</h1>
+        <div>
+          <div>DARK</div>
+          <img src={moon} alt="moon" />
+        </div>
+      </div>
+
+      <form>
+        <img src={search} alt="search" />
+        <input type="text" placeholder="Search Github username..." />
+        <button type="button">Search</button>
+      </form>
     </div>
   );
 }
