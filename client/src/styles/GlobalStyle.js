9c8dfdbe590a5f7f779231cc4e56f73e75920a3b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-07 15:57:58 -0400

    Create global styles

---
+++ client/src/styles/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components/macro";
+// import fonts from './fonts';
+import variables from "./variables";
+
+const GlobalStyle = createGlobalStyle`
+    /* ${fonts} */
+    ${variables}
+
+    html {
+        box-sizing: border-box;
+    }
+
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+`;
