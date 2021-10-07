47e07e4d2762d6cd12d6a1bf20836aad7145561f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-07 15:54:26 -0400

    Add Styles directory

---
+++ client/src/styles/variables.js
@@ -0,0 +1,29 @@
+import { css } from "styled-components/macro";
+
+const variables = css`
+  :root {
+    --light-blue: #0079ff;
+    --blue: #4b6a9b;
+    --dark-blue: #1e2a47;
+    --white: #ffffff;
+    --coconut: #fefefe;
+    --zircon: #f6f8ff;
+    --light-gray: #697c9a;
+    --dark-gray: #2b3442;
+    --black: #000000;
+    --red: rgba(246, 69, 69, 1);
+
+    --font: "Space Mono", --apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif;
+
+    --fs-xs: 11px;
+    --fs-sm: 13px;
+    --fs-md: 15px;
+    --fs-lg: 16px;
+    --fs-xl: 18px;
+    --fs-xxl: 22px;
+    --fs-xxxl: 26px;
+  }
+`;
+
+export default variables;
