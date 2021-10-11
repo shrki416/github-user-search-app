59389e6475d891d07fe5526b49412120304fc130
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-11 19:16:28 -0400

    Add and import the correct styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,76 @@ import moon from "./assets/icon-moon.svg";
 import search from "./assets/icon-search.svg";
 
 import { GlobalStyle } from "./styles";
+import { StyledTitle } from "./styles";
+import { StyledForm } from "./styles";
+
+import octocat from "./assets/octocat.png";
+import location from "./assets/icon-location.svg";
+import twitter from "./assets/icon-twitter.svg";
+import website from "./assets/icon-website.svg";
+import company from "./assets/icon-company.svg";
 
 function App() {
   return (
-    <div className="container">
+    <div className="app">
       <GlobalStyle />
-      <div>
+      <StyledTitle>
         <h1>devfinder</h1>
-        <div>
-          <div>DARK</div>
-          <img src={moon} alt="moon" />
+        <div className="toggle-mode">
+          <div className="mode">DARK</div>
+          <img src={moon} alt="moon" className="mode-logo" />
         </div>
-      </div>
+      </StyledTitle>
 
-      <form>
+      <StyledForm>
         <img src={search} alt="search" />
         <input type="text" placeholder="Search Github username..." />
         <button type="button">Search</button>
-      </form>
+      </StyledForm>
+
+      <div>
+        <div>
+          <img src={octocat} alt="octocat" />
+        </div>
+        <div>
+          <h1>The Octocat</h1>
+          <p>Joined 25 jan 2011</p>
+          <h3>@octocat</h3>
+          <p>This profile has no bio</p>
+        </div>
+        <div>
+          <div>
+            <h4>Repos</h4>
+            <span>8</span>
+          </div>
+          <div>
+            <h4>Followers</h4>
+            <span>3938</span>
+          </div>
+          <div>
+            <h4>Following</h4>
+            <span>9</span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src={location} alt="location" />
+            <p>San Fransico</p>
+          </div>
+          <div>
+            <img src={twitter} alt="twitter" />
+            <p>Not Available</p>
+          </div>
+          <div>
+            <img src={website} alt="website" />
+            <p>https://github.blog</p>
+          </div>
+          <div>
+            <img src={company} alt="company" />
+            <p>@github</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
