e36a4623dee9584086fd14c7d4f8690ade07f347
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-11 19:13:11 -0400

    Add Title styles

---
+++ client/src/styles/Title.js
@@ -0,0 +1,43 @@
+import styled from "styled-components/macro";
+
+const StyledTitle = styled.div`
+  align-items: center;
+  display: flex;
+  height: 38px;
+  min-width: 730px;
+
+  h1 {
+    letter-spacing: 0;
+    min-height: 38px;
+    min-width: 130px;
+    font-family: var(--font);
+  }
+
+  .toggle-mode {
+    display: flex;
+    align-items: flex-start;
+    margin-left: auto;
+    min-width: 78px;
+  }
+
+  .mode {
+    color: var(--light-gray);
+    font-family: var(--font);
+    font-size: var(--fs-sm);
+    font-weight: bold;
+    letter-spacing: 2.5px;
+    line-height: 15px;
+    margin-left: -2px;
+    min-height: 20px;
+    min-width: 46px;
+    text-align: right;
+  }
+
+  .mode-logo {
+    height: 20px;
+    margin-left: 14px;
+    width: 20px;
+  }
+`;
+
+export default StyledTitle;
