0550b0b560d8c27762fdd61a25da4c7136bbb2f6
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-12 17:53:28 -0400

    Add margin to the form

--- client/src/styles/StyledForm.js
+++ client/src/styles/StyledForm.js
@@ -1,15 +1,17 @@
 import styled from "styled-components/macro";
 
 const styledForm = styled.form`
+  background-color: var(--white);
   display: flex;
   align-items: flex-start;
   height: 109px;
   justify-content: flex-end;
-  min-width: 730px;
+  min-width: 770px;
   padding: 14px;
   border: 1px solid #e6e6e6;
   border-radius: 15px;
   margin-top: 20px;
+  box-shadow: 0px 0px 10px rgba(105, 124, 154, 0.1);
 
   img {
     height: 24px;
@@ -28,7 +30,6 @@ const styledForm = styled.form`
     border: none;
     font-family: var(--font);
     font-size: var(--fs-xl);
-    background-color: var(--zircon);
   }
 
   input::placeholder {
@@ -56,6 +57,11 @@ const styledForm = styled.form`
     min-width: 106px;
     padding: 12px 21px;
     border: 0;
+  }
+
+  input:hover,
+  button:hover {
+    cursor: pointer;
   }
 `;
 
