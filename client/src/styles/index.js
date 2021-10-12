96cb1e6759352ea1f7713f38cfc46b19646f6cb6
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-12 17:54:17 -0400

    Card Styles WIP

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,3 +1,4 @@
 export { default as GlobalStyle } from "./GlobalStyle";
 export { default as StyledTitle } from "./Title";
 export { default as StyledForm } from "./StyledForm";
+export { default as StyledCard } from "./StyledCard";
