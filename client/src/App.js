02fbc15125282c6e12adf69380cff0e71a3a3c04
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-01 15:12:20 -0500

    Restart styling process section by section

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,32 @@
-import moon from "./assets/icon-moon.svg";
-import search from "./assets/icon-search.svg";
-
-import { GlobalStyle, StyledTitle, StyledForm, StyledCard } from "./styles";
-
-import octocat from "./assets/octocat.png";
+import company from "./assets/icon-company.svg";
 import location from "./assets/icon-location.svg";
+import moon from "./assets/icon-moon.svg";
+import octocat from "./assets/octocat.png";
+import search from "./assets/icon-search.svg";
 import twitter from "./assets/icon-twitter.svg";
 import website from "./assets/icon-website.svg";
-import company from "./assets/icon-company.svg";
 
 function App() {
   return (
     <div className="app">
-      <GlobalStyle />
-      <StyledTitle>
-        <h1>devfinder</h1>
-        <div className="toggle-mode">
-          <div className="mode">DARK</div>
-          <img src={moon} alt="moon" className="mode-logo" />
+      <div className="main">
+        <div className="header">
+            <h1>devfinder</h1>
+            <div className="dark-mode">
+                <p>DARK</p>
+                <img src={moon} alt="dark theme button" />
+            </div>
         </div>
-      </StyledTitle>
 
-      <StyledForm>
-        <img src={search} alt="search" />
-        <input type="text" placeholder="Search Github username..." />
-        <button type="button">Search</button>
-      </StyledForm>
+        <div className="search" id="search-container">
+            <img src={search} alt="magnifying glass" />
+            <input type="text" id="input" placeholder='Search Github username...'/>
+            <div className="search-btn">
+                <button id="search">Search</button>
+            </div>
+        </div>
 
-      <StyledCard>
-        <div className="img">
-          <img src={octocat} alt="octocat" className="profile-img" />
-        </div>
-        <div className="bio">
-          <h1>The Octocat</h1>
-          <p>Joined 25 jan 2011</p>
-          <h3>@octocat</h3>
-          <p className="bio-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ipsa
-            esse laudantium vitae non nobis optio nemo facilis. Quas, vero!
-          </p>
-        </div>
-        <div className="stats">
-          <div>
-            <h4>Repos</h4>
-            <span>8</span>
-          </div>
-          <div>
-            <h4>Followers</h4>
-            <span>3938</span>
-          </div>
-          <div>
-            <h4>Following</h4>
-            <span>9</span>
-          </div>
-        </div>
-        <div className="social">
-          <div>
-            <img src={location} alt="location" />
-            <p>San Fransico</p>
-          </div>
-          <div>
-            <img src={twitter} alt="twitter" />
-            <p>Not Available</p>
-          </div>
-          <div>
-            <img src={website} alt="website" />
-            <p>https://github.blog</p>
-          </div>
-          <div>
-            <img src={company} alt="company" />
-            <p>@github</p>
-          </div>
-        </div>
-      </StyledCard>
+      </div>
     </div>
   );
 }
