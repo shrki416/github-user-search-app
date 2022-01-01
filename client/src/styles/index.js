caba035dca56b1d8902fd1078df153774259fccd
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-01 15:13:51 -0500

    Delete unused files

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,4 +1,1 @@
 export { default as GlobalStyle } from "./GlobalStyle";
-export { default as StyledTitle } from "./Title";
-export { default as StyledForm } from "./StyledForm";
-export { default as StyledCard } from "./StyledCard";
