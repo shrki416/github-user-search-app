0d6abf95d98e51542a6a60ad2a3a9ac6acc3ae77
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-04 14:00:32 -0500

    Log that server has started

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server Started on port: ${PORT}`));
