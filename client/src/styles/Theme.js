1f32689b263b9fc329687f5884ba88d3731b6c05
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-07 16:43:20 -0500

    Refactor and clean-up

--- client/src/styles/Theme.js
+++ client/src/styles/Theme.js
@@ -2,10 +2,12 @@ export const lightTheme = {
   body: "#f2f2f2",
   icon: "#697C9A",
   text: "#697C9A",
+  h1: "#222731",
 };
 
 export const darkTheme = {
   body: "#141D2F",
-  icon: '#ffffff',
-  text: '#ffffff'
+  icon: `var(--white)`,
+  text: `var(--white)`,
+  h1: `var(--white)`,
 }
