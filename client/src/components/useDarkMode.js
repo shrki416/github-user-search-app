94d75c02aef8c5921f9374a07f9c2a9a6b3be276
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-07 16:13:44 -0500

    Add custom hook for darktheme

---
+++ client/src/components/useDarkMode.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+export const useDarkMode = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    theme === 'light' ? setMode('dark') : setMode('light');
+  }
+
+  const setMode = (mode) => {
+    localStorage.setItem('theme', mode);
+    setTheme(mode);
+  }
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme');
+    localTheme ? setTheme(localTheme) : setMode('light');
+  }, []);
+
+  return [theme, toggleTheme];
+}
