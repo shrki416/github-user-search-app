bc711ffc1c9e2571eaff73bff7166c8285e22737
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-07 12:26:28 -0500

    Fix font weight values

--- client/src/styles/fonts.js
+++ client/src/styles/fonts.js
@@ -3,14 +3,14 @@ import { css } from "styled-components/macro";
 const fonts = css`
   @font-face {
     font-family: "Space Mono";
-    font-weight: bold;
+    font-weight: 400;
     font-style: normal;
     src: url("../assets/fonts/SpaceMono-Bold.ttf") format("ttf");
   }
 
   @font-face {
     font-family: "Space Mono";
-    font-weight: normal;
+    font-weight: 700;
     font-style: normal;
     src: url("../assets/fonts/SpaceMono-Regular.ttf") format("ttf");
   }
