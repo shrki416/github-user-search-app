d0c1ed3df4baeafc59540c8e771d261f5e9dd46b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-07 16:13:25 -0500

    Change colors based on theme

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { StyledHeader } from '../styles';
-import moon from "../assets/icon-moon.svg";
+import { ReactComponent as Moon } from "../assets/icon-moon.svg";
+import { StyledHeader } from "../styles";
+import { ReactComponent as Sun } from "../assets/icon-sun.svg";
 
-function Header() {
+function Header({ theme, toggleTheme }) {
   return (
     <StyledHeader>
       <h1>devfinder</h1>
       <div className="dark-mode">
-        <p>DARK</p>
-        <img src={moon} alt="dark theme button" />
+        <p>{theme === "light" ? "DARK" : "LIGHT"}</p>
+        <div onClick={toggleTheme}>
+          {theme === "light" ? <Moon /> : <Sun />}
+        </div>
       </div>
     </StyledHeader>
   );
