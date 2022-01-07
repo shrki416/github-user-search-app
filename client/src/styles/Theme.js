afcadb02b53f5aaeba26c5a8935963f450c9a92b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-07 16:14:19 -0500

    Create a themes file to update styles accordingly

---
+++ client/src/styles/Theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  body: "#f2f2f2",
+  icon: "#697C9A",
+  text: "#697C9A",
+};
+
+export const darkTheme = {
+  body: "#141D2F",
+  icon: '#ffffff',
+  text: '#ffffff'
+}
