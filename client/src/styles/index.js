20a9e5995a1cd900219f4a9dc7a472e5ea9f3c70
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-07 13:49:53 -0500

    Refactor and clean-up

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,3 +1,5 @@
 export { default as GlobalStyle } from "./GlobalStyle";
 export { default as Container } from "./Container";
 export { default as Main } from "./Main";
+export { default as StyledHeader } from "./StyledHeader";
+export { default as StyledSearch } from "./StyledSearch";
