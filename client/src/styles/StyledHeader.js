5af52b88e37fbe32ed2057d639cbfa8ec6bfb4e1
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-07 13:50:35 -0500

    Add Refactor header styles

---
+++ client/src/styles/StyledHeader.js
@@ -0,0 +1,32 @@
+import styled from "styled-components/macro";
+
+const StyledHeader = styled.header`
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+  h1 {
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 38.51px;
+  }
+
+  .dark-mode {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 100%;
+  }
+
+  .dark-mode img {
+    margin-left: 16px;
+  }
+
+  .dark-mode p {
+    font-size: 13px;
+    line-height: 19.25px;
+    letter-spacing: 2.5px;
+  }
+`;
+
+export default StyledHeader;
