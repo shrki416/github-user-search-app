c2e4bf2035fd59d15c2edca8c66499ea01c90d3a
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-09 16:49:37 -0500

    Add more to dark-theme: wip

--- client/src/styles/Theme.js
+++ client/src/styles/Theme.js
@@ -1,17 +1,21 @@
 export const lightTheme = {
   body: "#f2f2f2",
-  icon: "#697C9A",
-  text: "#697C9A",
+  icon: `var(--blue-grey)`,
+  text: `var(--blue-grey)`,
   h1: "#222731",
   bg: "#FEFEFE",
   searchText: "#4b6a9b",
+  userStatsBg: "#F6F8FF",
+  userCardHeader: "#2B3442",
 };
 
 export const darkTheme = {
-  body: "#141D2F",
+  body: `var(--dark-bg)`,
   icon: `var(--white)`,
   text: `var(--white)`,
   h1: `var(--white)`,
   bg: "#1E2A47",
   searchText: `var(--white)`,
-}
+  userStatsBg: `var(--dark-bg)`,
+  userCardHeader: `var(--white)`,
+};
