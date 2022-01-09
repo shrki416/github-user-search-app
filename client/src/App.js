20ea38c13c2b72e939e2412461d24f44a460bf3a
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-09 16:47:57 -0500

    Move main functionality from Card to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import { Container, GlobalStyle, Main } from "./styles";
 import { darkTheme, lightTheme } from "./styles/Theme";
 
+import API from "./api/api";
+import Card from "./components/Card";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import { ThemeProvider } from "styled-components";
-import company from "./assets/icon-company.svg";
-import location from "./assets/icon-location.svg";
-import octocat from "./assets/octocat.png";
-import twitter from "./assets/icon-twitter.svg";
 import { useDarkMode } from "./components/useDarkMode";
-import website from "./assets/icon-website.svg";
+import { useState } from "react";
 
 function App() {
   const [theme, toggleTheme] = useDarkMode();
+  const [search, setSearch] = useState("");
+  const [user, setUser] = useState({});
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    if (!search) return;
+    const response = await API.get(`/${search}`);
+    setUser(response.data);
+    setSearch("");
+  };
+
   return (
     <>
       <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
@@ -20,62 +29,12 @@ function App() {
         <Container>
           <Main>
             <Header theme={theme} toggleTheme={toggleTheme} />
-            <Search />
-
-            <div className="card">
-              <section className="card-img">
-                <img src={octocat} alt="octocat" />
-              </section>
-
-              <section className="card-body">
-                <div className="user-info">
-                  <h1 className="card-header">The Octocat</h1>
-                  <p className="user-name">@octocat</p>
-                  <p className="joined">Joined 25 Jan 2011</p>
-                </div>
-              </section>
-
-              <section className="user-bio">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Nihil, et.
-                </p>
-              </section>
-
-              <section className="user-stats">
-                <div>
-                  <p className="user-stats-header">Repos</p>
-                  <p className="user-stats-body">8</p>
-                </div>
-                <div>
-                  <p className="user-stats-header">Followers</p>
-                  <p className="user-stats-body">3938</p>
-                </div>
-                <div>
-                  <p className="user-stats-header">Following</p>
-                  <p className="user-stats-body">9</p>
-                </div>
-              </section>
-
-              <section className="social">
-                <div>
-                  <img src={location} alt="location" />
-                  <p>San Fransisco</p>
-                </div>
-                <div>
-                  <img src={twitter} alt="twitter" />
-                  <p>Not Available</p>
-                </div>
-                <div>
-                  <img src={website} alt="website" />
-                  <p>https://github.blog</p>
-                </div>
-                <div>
-                  <img src={company} alt="company" />
-                  <p>@github</p>
-                </div>
-              </section>
-            </div>
+            <Search
+              search={search}
+              setSearch={setSearch}
+              submit={handleFormSubmit}
+            />
+            <Card user={user} />
           </Main>
         </Container>
       </ThemeProvider>
