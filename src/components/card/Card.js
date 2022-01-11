69858cb2b5290866698ed8855209870d085834a0
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-11 12:05:23 -0500

    Refactor and clean-up

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,7 @@
 import { CardStyles } from "../../styles";
-import company from "../../assets/icon-company.svg";
+import Social from './Social';
 import formatDate from "../../lib/formatDate";
-import location from "../../assets/icon-location.svg";
 import octocat from "../../assets/octocat.png";
-import twitter from "../../assets/icon-twitter.svg";
-import website from "../../assets/icon-website.svg";
 
 function Card({ user }) {
   return (
@@ -48,34 +45,7 @@ function Card({ user }) {
         </div>
       </section>
 
-      <section className="social">
-        <div>
-          <img src={location} alt="location" />
-          <p id="location" className={`${!user.location && `not-available`}`}>
-            {user.location ? user.location : `Not Available`}
-          </p>
-        </div>
-        <div>
-          <img src={twitter} alt="twitter" />
-          <p className={`${!user.twitter_username && `not-available`}`}>
-            {user.twitter_username ? user.twitter_username : `Not Available`}
-          </p>
-        </div>
-        <div>
-          <img src={website} alt="website" />
-          <p className={`${!user.blog && `not-available`}`}>
-            <a href={`https://${user.blog}`} target="_blank" rel="noreferrer">
-              {user.blog ? user.blog : `Not Available`}
-            </a>
-          </p>
-        </div>
-        <div>
-          <img src={company} alt="company" />
-          <p className={`${!user.company && `not-available`}`}>
-            {user.company ? user.company : `Not Available`}
-          </p>
-        </div>
-      </section>
+      <Social user={user} />
     </CardStyles>
   );
 }
